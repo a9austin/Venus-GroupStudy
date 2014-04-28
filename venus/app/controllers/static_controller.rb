18108ac295d518812f4160be8bda18c15c164c51
Austin Truong <a9austin@gmail.com>
2014-04-28 01:06:26 -0600

    Added new contorller for User, and static pages, added a few updates to the gemfile

--- venus/app/controllers/static_controller.rb
+++ venus/app/controllers/static_controller.rb
@@ -1,2 +1,5 @@
 class StaticController < ApplicationController
+  def home
+    
+  end
 end
--- venus/config/routes.rb
+++ venus/config/routes.rb
@@ -3,7 +3,8 @@ Venus::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#home'
+  match '/signup', to: 'users#new', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
