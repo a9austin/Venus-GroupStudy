88d4c67625785f0203d78e2e1cd0f218816f73b9
Austin Truong <a9austin@gmail.com>
2014-04-28 00:41:14 -0600

    Created new rails application

---
+++ venus/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Venus::Application.config.secret_key_base = 'c79cb1c4248b71cd30dc43b26e533aaaf58f0fc50ebec1cd04964761224129f988de2b5b071d3af02a3a0ed4d966ccb207f583fe41f92a83c54bcb64c0f121a7'
---
+++ venus/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Venus::Application.config.session_store :cookie_store, key: '_venus_session'
